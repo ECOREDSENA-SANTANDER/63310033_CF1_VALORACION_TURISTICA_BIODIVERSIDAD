92c23a9e7a4f855ca2a13adedacc63a70a24c425
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-05-12 09:56:19 -0500

    Avance t1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,7 +7,7 @@
           span 1
         h1 ¿Qué es la biodiversidad? 
 
-      .row.justify-content-center.mb-5
+      .row.justify-content-center.mb-4
         .col-lg-9.col-md-12.col-sm-12
           .tarjeta--container.borde-recto.row
             .col-lg-5.tarjeta.bgterciario.px-3.py-2(data-aos="fade-right")
@@ -20,6 +20,118 @@
         .col-lg-3.col-md-6.col-12
           img.mt-0(src='@/assets/curso/t1/img2.png', alt='')
         
+      .row.align-items-center.justify-content-center.fondo1.mb-4
+        .col-sm-12.col-md-12.col-lg-10
+          PasosA.color-acento-contenido.mb-0(tipo="l")
+            .row.align-items-center.justify-content-center
+              .col-lg
+                p.mb-0 La biodiversidad es patrimonio de la nación y constituye un valor estratégico para el desarrollo presente y futuro de Colombia.
+              .col-auto
+                figure(data-aos="zoom-in")
+                  img.d-none.d-lg-block(src='@/assets/curso/t1/ps1-1.png', alt='')
+            .row.align-items-center.justify-content-center
+              .col-auto
+                figure(data-aos="zoom-in")
+                  img.d-none.d-lg-block(src='@/assets/curso/t1/ps1-2.png', alt='')
+              .col-lg
+                p Sus componentes tangibles incluyen moléculas, genes, poblaciones, especies, comunidades, ecosistemas y paisajes. También existen componentes intangibles, como los conocimientos, innovaciones y prácticas culturales asociadas a ella.
+            .row.align-items-center.justify-content-center
+              .col-lg
+                p.mb-0 La biodiversidad es dinámica, cambia a lo largo del tiempo y del espacio, y tanto sus componentes como sus procesos evolutivos deben ser conservados.
+              .col-auto
+                figure(data-aos="zoom-in")
+                  img.d-none.d-lg-block(src='@/assets/curso/t1/ps1-3.png', alt='')
+            .row.align-items-center.justify-content-center
+              .col-auto
+                figure(data-aos="zoom-in")
+                  img.d-none.d-lg-block(src='@/assets/curso/t1/ps1-4.png', alt='')
+              .col-lg
+                p Los beneficios derivados del uso de la biodiversidad deben distribuirse de manera justa y equitativa, en concertación con las comunidades involucradas.
+            .row.align-items-center.justify-content-center
+              .col-lg
+                p.mb-0 La política reconoce la importancia de proteger los derechos de propiedad intelectual, tanto individuales como colectivos.
+              .col-auto
+                figure(data-aos="zoom-in")
+                  img.d-none.d-lg-block(src='@/assets/curso/t1/ps1-5.png', alt='')
+            .row.align-items-center.justify-content-center
+              .col-auto
+                figure(data-aos="zoom-in")
+                  img.d-none.d-lg-block(src='@/assets/curso/t1/ps1-6.png', alt='')
+              .col-lg
+                p La conservación y el uso sostenible de la biodiversidad deben abordarse desde una perspectiva global, que requiere del compromiso internacional entre naciones.
+            .row.align-items-center.justify-content-center
+              .col-lg
+                p.mb-0 Es necesario un enfoque intersectorial y descentralizado, con participación del Estado en todos sus niveles y de la sociedad civil.
+              .col-auto
+                figure(data-aos="zoom-in")
+                  img.d-none.d-lg-block(src='@/assets/curso/t1/ps1-7.png', alt='')
+            .row.align-items-center.justify-content-center
+              .col-auto
+                figure(data-aos="zoom-in")
+                  img.d-none.d-lg-block(src='@/assets/curso/t1/ps1-8.png', alt='')
+              .col-lg
+                p Se aplicará el principio de precaución, especialmente en temas relacionados con la erosión genética y la bioseguridad.
+
+
+      Separador  
+      #t_1_1.titulo-segundo.color-acento-contenido(data-aos="zoom-in-left")
+        h2 1.1. Componentes de la biodiversidad en Colombia
+
+      p.mb-4 De acuerdo con la #[b Política Nacional para la Gestión Integral de la Biodiversidad y sus Servicios Ecosistémicos (PNGIBSE)], la biodiversidad en Colombia puede analizarse a partir de diversos componentes, dentro de los cuales encontrará:
+
+      .subt.mb-4(data-aos="fade-right")
+        .row.align-items-center
+          .col-md-auto.col-3
+            img(src='@/assets/curso/t1/ico.svg', alt='')
+          .col-md-auto.col-9.ps-0
+            h3.mb-0.text-white Ecosistemas y hábitats
+
+      p.mb-4 El hábitat es el entorno específico donde una especie vive y se desarrolla; la interacción entre especies y su entorno constituye un ecosistema, definido por la #[b Ley 165 de 1994] como:
+
+      .row.justify-content-center.align-items-center.mb-4
+        .col-lg-4.col-md-6.col-12
+          img(src='@/assets/curso/t1/img3.png', alt='')
+        .col-lg-8.col-md-12.col-sm-12
+          .row.bgverdeclaro
+            .bloque-texto-b.bgverdeclaro.p-4
+              .bloque-texto-b__texto
+                i.fas.fa-quote-left
+                h3.mb-0.text-regular Un complejo dinámico de comunidades vegetales, animales y de microorganismos, y su medio no viviente, que interactúan como una unidad funcional
+                i.fas.fa-quote-right
+            .row.justify-content-end.mt-0
+              .col-auto
+                img.mb-4(src='@/assets/curso/t1/img4.png', alt='')
+          .row.bgverde.borde-redondo.mt-3
+            p.p-4.mt-2 Otra definición señala que un ecosistema es un conjunto natural relativamente estable, en el cual las relaciones entre los seres vivos y las condiciones ambientales permiten su mantenimiento en el tiempo (Ministerio de Ambiente y Desarrollo Sostenible, 2017).
+
+      .row.justify-content-center.align-items-center.mb-4
+        .col-auto
+          img(src='@/assets/curso/t1/img4.png', alt='')
+        .col
+          p.mt-2 La diversidad de ecosistemas está directamente relacionada con la influencia de las actividades humanas. Según la historia de ocupación y uso del territorio, se pueden distinguir desde ecosistemas naturales apenas alterados por el ser humano, hasta ecosistemas profundamente transformados por la actividad humana.
+
+      p El Informe nacional sobre el estado de la biodiversidad distingue dos tipos de diversidad ecosistémica:
+
+      .row.justify-content-center.mb-4
+        .col-lg-12.col-md-12.col-sm-12
+          .tarjeta--container.row
+            .col-lg-6.tarjeta.color-primario.px-3.py-2(data-aos="fade-right")
+              .row.justify-content-center.p-4
+                .col-lg-3
+                  img.img-a.img-t(src='@/assets/curso/t1/tar1-1.png', alt='')
+                .col-lg-7
+                  .row.bgverdeoscuro.mx-1.mt-4
+                    h2.mt-1.mb-1 Diversidad ecosistémica original o potencial
+              p.mb-0.px-5 Corresponde al estado de los ecosistemas antes de la intervención humana. Aunque algunos han sido modificados, pueden ser restaurados a partir del conocimiento de las geoformas, suelos, clima, vegetación y registros históricos.
+            .col-lg-6.tarjeta.color-secundario.px-3.py-2(data-aos="fade-right")
+              .row.justify-content-center.p-4
+                .col-lg-3
+                  img.img-a.img-t(src='@/assets/curso/t1/tar1-2.png', alt='')
+                .col-lg-7
+                  .row.bgverdeoscuro.mx-1.mt-4
+                    h2.mt-1.mb-1.text-white Diversidad ecosistémica actual
+              p.mb-0.px-5 Resulta de la interacción entre la diversidad original y los procesos naturales con las actividades humanas a lo largo del tiempo.
+
 
   </template>
 
